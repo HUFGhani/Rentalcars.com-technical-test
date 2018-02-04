bfa3afe5e8a200c0c0a31134384b903a47d0cab8
Hamza Ghani <h.u.f.ghani@gmail.com>
2018-02-04 16:25:58 +0000

    Add more test data to start up and fix testFormatPriceOrder (Red)

--- RentalCarsPart1/src/test/java/io/github/hufghani/formatter/VehicleOutputFormatTest.java
+++ RentalCarsPart1/src/test/java/io/github/hufghani/formatter/VehicleOutputFormatTest.java
@@ -17,35 +17,51 @@ public class VehicleOutputFormatTest {
 
     @Before
     public void setUp() throws Exception {
-        VehicleList vehicle = new VehicleList();
-        vehicle.setName("Vehicle 1");
-        vehicle.setPrice(100.0);
-        vehicle.setRating(10.0);
-        vehicle.setSipp("MBMN");
-        vehicle.setSupplier("Supplier 1");
-        vehicle.setScore(11.0);
+        VehicleList vehicle1 = new VehicleList();
+        vehicle1.setName("Vehicle 1");
+        vehicle1.setPrice(100.0);
+        vehicle1.setRating(10.0);
+        vehicle1.setSipp("MBMN");
+        vehicle1.setSupplier("Supplier 1");
+        vehicle1.setScore(11.0);
+        VehicleSpecification vehicleSpecification1 = new VehicleSpecification();
+        vehicleSpecification1.setAc("no AC");
+        vehicleSpecification1.setVehicleList(vehicle1);
+        vehicleSpecification1.setCarType("Mini");
+        vehicleSpecification1.setDoors("2 doors");
+        vehicleSpecification1.setFuel("Petrol");
+        vehicleSpecification1.setTransmission("Manual");
+
+        VehicleList vehicle2 = new VehicleList();
+        vehicle2.setName("Vehicle 2");
+        vehicle2.setPrice(600.0);
+        vehicle2.setRating(10.0);
+        vehicle2.setSipp("MBMN");
+        vehicle2.setSupplier("Supplier 1");
+        vehicle2.setScore(11.0);
+        VehicleSpecification vehicleSpecification2 = new VehicleSpecification();
+        vehicleSpecification2.setAc("no AC");
+        vehicleSpecification2.setVehicleList(vehicle1);
+        vehicleSpecification2.setCarType("Mini");
+        vehicleSpecification2.setDoors("2 doors");
+        vehicleSpecification2.setFuel("Petrol");
+        vehicleSpecification2.setTransmission("Manual");
+
 
         vehicles = new ArrayList<>();
-        vehicles.add(vehicle);
-
-        VehicleSpecification vehicleSpecification = new VehicleSpecification();
-        vehicleSpecification.setAc("no AC");
-        vehicleSpecification.setVehicleList(vehicle);
-        vehicleSpecification.setCarType("Mini");
-        vehicleSpecification.setDoors("2 doors");
-        vehicleSpecification.setFuel("Petrol");
-        vehicleSpecification.setTransmission("Manual");
-
         vehicleSpecifications = new ArrayList<>();
-        this.vehicleSpecifications.add(vehicleSpecification);
-
-        this.vehicleOutputFormat = new VehicleOutputFormat();
+        vehicles.add(vehicle1);
+        vehicles.add(vehicle2);
+        vehicleSpecifications.add(vehicleSpecification1);
+        vehicleSpecifications.add(vehicleSpecification2);
+        vehicleOutputFormat = new VehicleOutputFormat();
     }
 
 
     @Test
     public void testFormatPriceOrder() {
         String output = vehicleOutputFormat.formatPriceOrder(vehicles);
-        Assert.assertEquals("1. Car 1 - 100,00" + LINE_SEPARATOR,output);
+        Assert.assertEquals("1. Vehicle 1 - 100.00" + LINE_SEPARATOR +"2. Vehicle 2 - 600.00"+LINE_SEPARATOR,output);
+        System.out.println(output);
     }
 }
