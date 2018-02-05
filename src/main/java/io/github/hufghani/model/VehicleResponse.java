ede24107696ac3a120d0f9c2c6c71c91c381e1c0
Hamza Ghani <h.u.f.ghani@gmail.com>
2018-02-05 21:11:04 +0000

    Code Passed Test testGetScore (Green)

---
+++ src/main/java/io/github/hufghani/model/VehicleResponse.java
@@ -0,0 +1,35 @@
+package io.github.hufghani.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class VehicleResponse implements Serializable {
+
+    @JsonProperty("Result")
+    private Object result;
+
+    public VehicleResponse() {
+    }
+
+    public VehicleResponse(Object result) {
+        super();
+        this.result = result;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public VehicleResponse setResult(Object result) {
+        this.result = result;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "VehicleResponse{" +
+                "result=" + result +
+                '}';
+    }
+}
