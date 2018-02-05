1e6171ecfb783d27568221af1898407544959b3e
Hamza Ghani <h.u.f.ghani@gmail.com>
2018-02-05 21:03:38 +0000

    Code Failed Test testGetScore (Red)

---
+++ src/test/java/io/github/hufghani/controller/VehicleControllerTest.java
@@ -0,0 +1,52 @@
+package io.github.hufghani.controller;
+
+import io.github.hufghani.formatter.VehicleOutputFormat;
+import io.github.hufghani.model.VehicleResponse;
+import io.github.hufghani.service.VehicleService;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+
+@RunWith(MockitoJUnitRunner.class)
+public class VehicleControllerTest {
+
+    private VehicleController vehicleController;
+    @Mock
+    private VehicleService vehicleService;
+    @Mock
+    private VehicleOutputFormat vehicleOutputFormat;
+
+    @Before
+    public void setUp() throws Exception {
+
+        vehicleController = new VehicleController();
+
+        Mockito.reset(vehicleService,vehicleOutputFormat);
+
+        try {
+            ReflectionTestUtils.setField(vehicleController,"vehicleService",vehicleService,VehicleService.class);
+            ReflectionTestUtils.setField(vehicleController,"vehicleOutputFormat",vehicleOutputFormat,VehicleOutputFormat.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testGetScore() {
+        Mockito.when(vehicleOutputFormat.formatCalculateCombineScore(Mockito.anyList())).thenReturn("Test");
+
+        ResponseEntity<VehicleResponse> result = vehicleController.getScore();
+
+        Mockito.verify(vehicleService, Mockito.times(1)).calculateCombineScore();
+        Assert.assertEquals(200, result.getStatusCodeValue());
+        Assert.assertEquals("Test", result.getBody().getResult());
+    }
+
+
+}
